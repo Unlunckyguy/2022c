95617eb3d1c6844174f9a84f162217c978ce87e9
James <chiu94545@gmail.com>
2022-12-16 11:56:56 +0800

    week15

---
+++ week15/week15-1.cpp
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+ int main()
+ {
+    printf("Hellp World\n");
+
+    char line[]= "Hello World";
+
+    printf("整數%d\n",100);
+    printf("浮點數%f\n",3.1415216841654864);
+    printf("%s字串\n",line);
+ }
